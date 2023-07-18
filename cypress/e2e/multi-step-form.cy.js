87717ca17214257984faac0cf2affea7e3f2a716
Artur Iskhakov <sizieks@gmail.com>
2023-07-19 04:58:25 +0500

    test: add a more detailed test

--- cypress/e2e/multi-step-form.cy.js
+++ cypress/e2e/multi-step-form.cy.js
@@ -1,38 +1,138 @@
-describe('multi-step form', () => {
-  it('info', () => {
-    const name = 'John Doe';
-    const email = 'jd@lorem.ipsum';
-    const phone = '+1234567890';
+const user = {
+  name: 'John Doe',
+  email: 'jd@lorem.ipsum',
+  phone: '+1234567890',
+};
 
+const isStepVisible = (currentStep) => {
+  const steps = [
+    'Personal info',
+    'Select your plan',
+    'Pick add-ons',
+    'Finishing up',
+    'Thank you!',
+  ];
+
+  steps.forEach((step) => {
+    steps.indexOf(step) === currentStep
+      ? cy.contains(step).should('be.visible')
+      : cy.contains(step).should('not.be.visible');
+  });
+};
+
+const checkStepStatus = (step, status) => {
+  cy.contains(`Step ${1 + step}`)
+    .parent()
+    .parent()
+    .parent()
+    .should('have.class', `ant-steps-item-${status}`);
+};
+
+describe('multi-step form', () => {
+  it('fills and submits form', () => {
     cy.visit('http://localhost:5173/');
-    cy.contains('Personal info');
-    // cy.get('[data-cy="step-1"]').should('not.be.visible')
-    cy.get('#form_item_name').type(name).should('have.value', name);
-    cy.get('#form_item_email').type(email).should('have.value', email);
-    cy.get('#form_item_phone').type(phone).should('have.value', phone);
+
+    isStepVisible(0);
+    for (let i = 0; i < 4; i++) {
+      i === 0 ? checkStepStatus(i, 'active') : checkStepStatus(i, 'wait');
+    }
+    cy.contains('Backward').should('not.be.visible');
+    cy.contains('Forward').should('be.disabled');
+    cy.get('#form_item_name')
+      .type('1337 User')
+      .should('have.value', '1337 User');
+    cy.get('#form_item_email')
+      .type(user.email)
+      .should('have.value', user.email);
+    cy.get('#form_item_phone')
+      .type(user.phone)
+      .should('have.value', user.phone);
+    cy.contains('Name is required');
+    cy.contains('Forward').should('be.disabled');
+    cy.get('#form_item_name ~ span [role="button"]')
+      .click()
+      .should('have.value', '');
+    cy.get('#form_item_name').type(user.name).should('have.value', user.name);
     cy.contains('Forward').click();
-    cy.contains('Select your plan');
-    cy.get('[for="plan-2"]').click();
+
+    isStepVisible(1);
+    checkStepStatus(1, 'active');
+    checkStepStatus(0, 'finish');
+    cy.get('[for="plan-1"]')
+      .as('plan-1')
+      .children()
+      .should('have.class', 'active');
+    cy.get('[for="plan-2"]')
+      .as('plan-2')
+      .click()
+      .children()
+      .should('have.class', 'active');
     cy.contains('Forward').click();
-    cy.contains('Pick add-ons');
-    cy.get('#addon-1').click();
-    cy.get('#addon-3').click();
+
+    isStepVisible(2);
+    checkStepStatus(2, 'active');
+    checkStepStatus(1, 'finish');
+    cy.get('#addon-1').click().should('be.checked');
+    cy.get('#addon-3').click().should('be.checked');
     cy.contains('Forward').click();
-    cy.contains('Finishing up');
+
+    isStepVisible(3);
+    checkStepStatus(3, 'active');
+    checkStepStatus(2, 'finish');
+    cy.contains('Total (per month)');
     cy.contains('Change').click();
-    cy.contains('Select your plan');
-    cy.get('[for="plan-3"]').click();
-    cy.get('button[role="switch"]').click();
-    cy.contains('Forward').click();
-    cy.get('#addon-2').click();
-    cy.contains('Forward').click();
-    for (let i = 0; i < 3; i++) {
-      cy.contains('Backward').click();
+
+    isStepVisible(1);
+    for (let i = 0; i < 4; i++) {
+      i === 0
+        ? checkStepStatus(i, 'finish')
+        : i === 1
+        ? checkStepStatus(i, 'active')
+        : checkStepStatus(i, 'wait');
     }
-    for (let i = 0; i < 3; i++) {
-      cy.contains('Forward').click();
-    }
+    cy.get('@plan-2').children().should('have.class', 'active');
+    cy.get('[for="plan-3"]')
+      .as('plan-3')
+      .click()
+      .children()
+      .should('have.class', 'active');
+    cy.get('@plan-1').contains('$9');
+    cy.get('@plan-2').contains('$12');
+    cy.get('@plan-3').contains('$15');
+    cy.get('button[role="switch"]')
+      .should('have.attr', 'aria-checked', 'monthly')
+      .click()
+      .should('have.attr', 'aria-checked', 'yearly');
+    cy.get('@plan-1').contains('$90');
+    cy.get('@plan-2').contains('$120');
+    cy.get('@plan-3').contains('$150');
+    cy.contains('2 months free');
+    cy.contains('Forward').click();
+
+    isStepVisible(2);
+    checkStepStatus(2, 'active');
+    checkStepStatus(1, 'finish');
+    cy.get('#addon-1').should('be.checked');
+    cy.get('#addon-3').should('be.checked');
+    cy.get('#addon-2').click().should('be.checked');
+    cy.contains('Forward').click();
+
+    isStepVisible(3);
+    checkStepStatus(3, 'active');
+    checkStepStatus(2, 'finish');
+    cy.contains('Pro (Yearly)').siblings().contains('$150');
+    cy.contains('Online service').parent().contains('+$10');
+    cy.contains('Larger storage').parent().contains('+$20');
+    cy.contains('Customizable Profile').parent().contains('+$30');
+    cy.contains('Total (per year)').siblings().contains('$210');
+
+    cy.contains('Backward').click().click().click();
+    cy.contains('Forward').click().click().click();
     cy.contains('Confirm').click();
-    cy.contains('Thank you!');
+
+    isStepVisible(4);
+    checkStepStatus(3, 'finish');
+    cy.contains('Backward').should('not.be.visible');
+    cy.contains('Forward').should('not.be.visible');
   });
 });
