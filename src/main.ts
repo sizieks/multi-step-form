a6af51ddbe3f29e0c84c53e138f0952b40ec2e44
Artur Iskhakov <sizieks@gmail.com>
2023-10-23 18:02:50 +0500

    build: delete source code; add dist subtree

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import Antd from 'ant-design-vue';
-import './style.css';
-import App from './App.vue';
-import 'ant-design-vue/dist/reset.css';
-
-const app = createApp(App).use(Antd);
-app.mount('#app');
